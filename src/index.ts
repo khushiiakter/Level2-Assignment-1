7b7335ef49641979154c10510f4eb0e08db6b189
khushiiakter <rkjia1311@gmail.com>
2025-05-08 03:07:06 +0600

    solve problem 4

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,8 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 }
 
 
+
+
 class Vehicle {
     private make: string;
     private year: number;
@@ -28,7 +30,26 @@ class Vehicle {
         this.make = make;
         this.year = year; 
     }
+
+    getInfo(): string{
+        return  `Make: ${this.make}, Year: ${this.year}`
+    }
 }
+class Car extends Vehicle {
+    private model: string;
+
+    constructor(make: string, year: number, model: string){
+        super(make, year);
+        this.model = model;
+    }
+
+    getModel(): string{
+        return  `Model: ${this.model}`
+    }
+}
+const myCar = new Car("Toyota", 2020, "Corolla");
+
+
 
 
  
